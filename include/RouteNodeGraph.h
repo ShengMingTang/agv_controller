c8d20f0af7bf68febe02beeefde2e8e9bfb63ef8
shengming <shengming0308@gapp.nthu.edu.tw>
2019-10-09 22:08:50 +0800

    Graph halted

--- include/RouteNodeGraph.h
+++ include/RouteNodeGraph.h
@@ -1,44 +1,47 @@
-#ifndef ROUTENODEGRAPH_H
-#define ROUTENODEGRAPH_H
-#include <ros/ros.h>
-#include <vector>
-#include <list>
-#include "pybot.h"
-#include "wifi/RouteNode.h"
-#include <geometry_msgs/Pose.h>
-using namespace std;
-using namespace pybot;
-using namespace wifi;
-namespace pybot
-{
-    // RouteNode defined in wifi pkg
-    template<typename T>
-    struct Edge
-    {
-        Edge(T _dest, list<geometry_msgs::Quaternion> _path)
-        :v{_dest}
-        {
-            for(auto it : _path)
-                w += sqrt(it.x * it.x + it.y * it.y);
-        }
-        ~Edge() {}
-        T v; // this node, adjacent node
-        double w = 0; // weight
+// #ifndef ROUTENODEGRAPH_H
+// #define ROUTENODEGRAPH_H
+// #include <ros/ros.h>
+// #include <vector>
+// #include <list>
+// #include "pybot.h"
+// #include "wifi/RouteNode.h"
+// #include <geometry_msgs/Pose.h>
+// using namespace std;
+// using namespace pybot;
+// using namespace wifi;
+
+// // Need Further implementation
+
+// namespace pybot
+// {
+//     // RouteNode defined in wifi pkg
+//     template<typename T>
+//     struct Edge
+//     {
+//         Edge(const T& _dest, list<geometry_msgs::Quaternion>& _path)
+//         :v{_dest}
+//         {
+//             for(auto it : _path)
+//                 w += sqrt(it.x * it.x + it.y * it.y);
+//         }
+//         ~Edge() {}
+//         T v; // adjacent node
+//         double w = 0; // weight
         
-    };
-    template<typename T>
-    class Graph
-    {
-    public:
-        Graph() {}
-        ~Graph() {}
-        void add_node(T _added, Edge<T> _edge);
-        double cost_to_target(const T& _curr, const T& _target);
-        list<T> path_to_target(const T& _curr, const T& _target);
-        void clear();
-    private:
-        vector< vector<T> > nodes;
-        vector< vector<double> > weights;
-    };
-}
-#endif
+//     };
+//     template<typename T>
+//     class Graph
+//     {
+//     public:
+//         Graph() {}
+//         ~Graph() {}
+//         void add_node(const T& _added, list<geometry_msgs::Quaternion>& _path);
+//         double cost_to_target(const T& _curr, const T& _target);
+//         list<T> path_to_target(const T& _curr, const T& _target);
+//         void clear();
+//     private:
+//         vector< vector<T> > nodes;
+//         vector< vector<double> > weights;
+//     };
+// }
+// #endif
--- src/RouteNodeGraph.cpp
+++ src/RouteNodeGraph.cpp
@@ -1,51 +1,50 @@
-#include "RouteNodeGraph.h"
-// template specialization
-template<>
-void Graph<wifi::RouteNode>::add_node(wifi::RouteNode _added, Edge<wifi::RouteNode> _edge)
-{
-
-    if(_added.route >= 0){
-        while(this->nodes.size() <= _added.route){
-            this->nodes.push_back(vector<wifi::RouteNode>());
-            this->weights.push_back(vector<double>());
-        }
-        this->nodes[_added.route].push_back(_added);
-        this->weights[_added.route].push_back(_edge.w);
-        ROS_INFO("R:%d, N:%d, W:%.1f @ (%.1f, %.1f) added", _added.route, _added.node, _edge.w, _added.pos.x, _added.pos.y);
-    }
-    else{
-        ROS_ERROR("RouteNode Wifi Pass Error");
-    }
-}
-template<>
-double Graph<wifi::RouteNode>::cost_to_target(const wifi::RouteNode& _curr, const wifi::RouteNode& _target)
-{
-    double ret = 0;
-    // assume _curr -> origin -> _target
-    for(int i = _curr.node; i > 0; i--){
-        ret += this->weights[_curr.route][i];
-    }
-    for(int i = 0; i <= _target.node; i++){
-        ret += this->weights[_target.route][i];
-    }
-    return ret;
-}
-template<>
-list<wifi::RouteNode> Graph<wifi::RouteNode>::path_to_target(const wifi::RouteNode& _curr, const wifi::RouteNode& _target)
-{
-    list<wifi::RouteNode> ret;
-    // assume _curr -> origin -> _target
-    for(int i = _curr.node; i > 0; i--){
-        ret.push_back(this->nodes[_curr.route][i]);
-    }
-    for(int i = 0; i <= _target.node; i++){
-        ret.push_back(this->nodes[_target.route][i]);
-    }
-    return ret;
-}
-template<>
-void Graph<wifi::RouteNode>::clear()
-{
-    this->nodes.clear();
-    this->weights.clear();
-}
+// #include "RouteNodeGraph.h"
+// // template specialization on wifi::RouteNode
+// template<>
+// void Graph<wifi::RouteNode>::add_node(const wifi::RouteNode& _added, list<geometry_msgs::Quaternion>& _path)
+// {
+//     if(_added.route >= 0){
+//         while(this->nodes.size() <= _added.route){
+//             this->nodes.push_back(vector<wifi::RouteNode>());
+//             this->weights.push_back(vector<double>());
+//         }
+//         this->nodes[_added.route].push_back(_added);
+//         this->weights[_added.route].push_back(_edge.w);
+//         ROS_INFO("R:%d, N:%d, W:%.1f @ (%.1f, %.1f) added", _added.route, _added.node, _edge.w, _added.pos.x, _added.pos.y);
+//     }
+//     else{
+//         ROS_ERROR("Route <0 Error");
+//     }
+// }
+// template<>
+// double Graph<wifi::RouteNode>::cost_to_target(const wifi::RouteNode& _curr, const wifi::RouteNode& _target)
+// {
+//     double ret = 0;
+//     // assume _curr -> origin -> _target
+//     for(int i = _curr.node; i > 0; i--){
+//         ret += this->weights[_curr.route][i];
+//     }
+//     for(int i = 0; i <= _target.node; i++){
+//         ret += this->weights[_target.route][i];
+//     }
+//     return ret;
+// }
+// template<>
+// list<wifi::RouteNode> Graph<wifi::RouteNode>::path_to_target(const wifi::RouteNode& _curr, const wifi::RouteNode& _target)
+// {
+//     list<wifi::RouteNode> ret;
+//     // assume _curr -> origin -> _target
+//     for(int i = _curr.node; i > 0; i--){
+//         ret.push_back(this->nodes[_curr.route][i]);
+//     }
+//     for(int i = 0; i <= _target.node; i++){
+//         ret.push_back(this->nodes[_target.route][i]);
+//     }
+//     return ret;
+// }
+// template<>
+// void Graph<wifi::RouteNode>::clear()
+// {
+//     this->nodes.clear();
+//     this->weights.clear();
+// }
