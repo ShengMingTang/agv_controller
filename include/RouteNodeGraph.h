3d9249e66b482be08d0e8818f8929fefc3f37e0e
shengming <shengming0308@gapp.nthu.edu.tw>
2019-10-10 16:50:04 +0800

    graph started

--- include/RouteNodeGraph.h
+++ include/RouteNodeGraph.h
@@ -1,47 +1,44 @@
-// #ifndef ROUTENODEGRAPH_H
-// #define ROUTENODEGRAPH_H
-// #include <ros/ros.h>
-// #include <vector>
-// #include <list>
-// #include "pybot.h"
-// #include "wifi/RouteNode.h"
-// #include <geometry_msgs/Pose.h>
-// using namespace std;
-// using namespace pybot;
-// using namespace wifi;
+#ifndef ROUTENODEGRAPH_H
+#define ROUTENODEGRAPH_H
+#include <ros/ros.h>
+#include <vector>
+#include <list>
+#include <geometry_msgs/Pose.h>
+#include "pybot.h"
+#include "agv_controller/RouteNode.h"
+using namespace std;
+using namespace pybot;
 
-// // Need Further implementation
-
-// namespace pybot
-// {
-//     // RouteNode defined in wifi pkg
-//     template<typename T>
-//     struct Edge
-//     {
-//         Edge(const T& _dest, list<geometry_msgs::Quaternion>& _path)
-//         :v{_dest}
-//         {
-//             for(auto it : _path)
-//                 w += sqrt(it.x * it.x + it.y * it.y);
-//         }
-//         ~Edge() {}
-//         T v; // adjacent node
-//         double w = 0; // weight
-        
-//     };
-//     template<typename T>
-//     class Graph
-//     {
-//     public:
-//         Graph() {}
-//         ~Graph() {}
-//         void add_node(const T& _added, list<geometry_msgs::Quaternion>& _path);
-//         double cost_to_target(const T& _curr, const T& _target);
-//         list<T> path_to_target(const T& _curr, const T& _target);
-//         void clear();
-//     private:
-//         vector< vector<T> > nodes;
-//         vector< vector<double> > weights;
-//     };
-// }
-// #endif
+namespace pybot
+{
+    list<geometry_msgs::Quaternion> flip_path(list<geometry_msgs::Quaternion>& _path);
+    template<typename T>
+    struct Edge
+    {
+        Edge(const T& _dest, list<geometry_msgs::Quaternion>& _path)
+        :v{_dest}
+        {
+            for(auto it : _path)
+                w += sqrt(it.x * it.x + it.y * it.y);
+        }
+        ~Edge() {}
+        T v; // adjacent node
+        list<geometry_msgs::Quaternion> walk; // a series of motion to v
+        double w = 0; // weight
+    };
+    template<typename T>
+    class Graph
+    {
+    public:
+        Graph() {}
+        ~Graph() {}
+        void add_node(const T& _added, list<geometry_msgs::Quaternion>& _path);
+        double cost_to_target(const T& _curr, const T& _target);
+        list<T> path_to_target(const T& _curr, const T& _target);
+        void clear();
+    private:
+        vector< vector<T> > nodes;
+        vector< vector<double> > weights;
+    };
+}
+#endif
