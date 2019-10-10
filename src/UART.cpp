1f77e005b099b506b46780e21538e2722c967f69
shengming <shengming0308@gapp.nthu.edu.tw>
2019-10-10 13:08:37 +0800

    rearragned

--- src/UART.cpp
+++ src/UART.cpp
@@ -1,9 +1,13 @@
 #include "UART.h"
-UART::UART(const string& _parent_frame_id):
-invoke_clt{this->n.serviceClient<RobotInvoke>(ROBOTINVOKE_TOPIC)}
-,frame_id{_parent_frame_id + "/uart"}
+UART::UART(const string& _parent_frame_id)
+:frame_id {_parent_frame_id + "/uart"}
+,invoke_clt {this->n.serviceClient<RobotInvoke>(ROBOTINVOKE_TOPIC)}
 {
     ROS_INFO("UART constructed");
+}
+UART::~UART()
+{
+    ROS_INFO("UART destroyed");
 }
 RobotInvoke UART::invoke(const char _op, const std::vector<int16_t> _args)
 {
@@ -12,32 +16,30 @@ RobotInvoke UART::invoke(const char _op, const std::vector<int16_t> _args)
     srv.request.header.frame_id = this->frame_id;
     srv.request.operation = _op;
     srv.request.argument = _args;
-    bool is_success = this->invoke_clt.call(srv);
     if(!this->invoke_clt.call(srv))
-        ROS_ERROR("Connection to UART Service failed");
+        ROS_ERROR("|---X---> UART(Srv) failed");
     return srv;
-}
-UART::~UART()
-{
-    ROS_INFO("UART destroyed");
 }
 bool UART::is_invoke_valid(RobotInvoke _srv)
 {
     stringstream ss;
     bool ret = true;
-    ss << "Op = " << _srv.request.operation << " ";
+    ss << "Op = " << _srv.request.operation << ", ";
     if(!_srv.response.is_legal_op){
-        ss << "    : Operation illegal\n";
+        ss << "Op ill. , ";
         ret = false;
     }
     if(!_srv.response.is_arg_valid){
-        ss << "    : Args illegal\n";
+        ss << "Args ill. , ";
         ret = false;
     }
+    else
+
     if(!_srv.response.is_activated){
-        ss << "    : Connect to UART <-> AGV failed";
+        ss << "UART---X---> AGV fail, ";
         ret = false;
     }
+    ss << "Err: " << _srv.response.error_code;
     if(!ret)
         ROS_ERROR(ss.str().c_str());
     return ret;
