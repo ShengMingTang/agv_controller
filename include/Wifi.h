a7f0b8050ba11ba04b85561d8d617797672dc7b8
shengming <shengming0308@gapp.nthu.edu.tw>
2019-10-20 23:01:24 +0800

    not complete

--- include/Wifi.h
+++ include/Wifi.h
@@ -1,29 +1,33 @@
-// #ifndef WIFI_H
-// #define WIFI_H
-// #include <ros/ros.h>
-// #include <queue>
-// #include "pybot.h"
-// #include "wifi/WifiIO.h"
-// #include "wifi/WifiSend.h"
-// using namespace std;
-// using namespace pybot;
-// namespace pybot
-// {
-//     class Wifi
-//     {
-//     public:
-//         Wifi(const string& _parent_frame_id);
-//         ~Wifi();
-//         void send(wifi::WifiIO _msg);
-//         bool is_msg_valid(wifi::WifiIO::ConstPtr _msg);
-//         wifi::WifiIO::ConstPtr pop();
-//     private:
-//         void recv_callback(wifi::WifiIO::ConstPtr _msg);
-//         ros::NodeHandle n;
-//         const string frame_id;
-//         queue<wifi::WifiIO::ConstPtr> que;
-//         ros::ServiceClient send_clt; //send wifi
-//         ros::Subscriber recv_sub; // receiver
-//     };
-// }
-// #endif
+#ifndef WIFI_H
+#define WIFI_H
+#include <ros/ros.h>
+#include <queue>
+#include "pybot.h"
+#include "tircgo_msgs/WifiIO.h"
+#include "tircgo_msgs/WifiSend.h"
+using namespace std;
+using namespace pybot;
+using namespace tircgo_msgs;
+
+namespace pybot
+{
+    class Wifi
+    {
+    public:
+        Wifi(const string& _parent_frame_id);
+        ~Wifi();
+        void send(const string& _hint);
+        bool node_ocp(const RouteNode& _nd);
+        bool is_msg_valid(WifiIO::ConstPtr _msg);
+        WifiIO::ConstPtr pop();
+        bool empty()const {return this->que.empty();}
+    private:
+        void recv_callback(WifiIO::ConstPtr _msg);
+        ros::NodeHandle n;
+        const string frame_id;
+        queue<WifiIO::ConstPtr> que;
+        ros::ServiceClient send_clt; //send wifi
+        ros::Subscriber recv_sub; // receiver
+    };
+}
+#endif
--- src/Wifi.cpp
+++ src/Wifi.cpp
@@ -1,43 +1,54 @@
-// #include "Wifi.h"
+#include "Wifi.h"
 
-// Wifi::Wifi(const string& _parent_frame_id):
-// frame_id{_parent_frame_id + "/wifi"}
-// ,send_clt{this->n.serviceClient<wifi::WifiSend>(ROBOT_WIFI_SEND_SRV)}
-// ,recv_sub{this->n.subscribe(ROBOT_WIFI_TOPIC, MSG_QUE_SIZE, &Wifi::recv_callback, this)}
-// {
-//     ROS_INFO("Wifi constructed");
-// }
+Wifi::Wifi(const string& _parent_frame_id):
+frame_id{_parent_frame_id + "/wifi"}
+,send_clt{this->n.serviceClient<tircgo_msgs::WifiSend>(ROBOT_WIFI_SEND_SRV)}
+,recv_sub{this->n.subscribe(ROBOT_WIFI_TOPIC, MSG_QUE_SIZE, &Wifi::recv_callback, this)}
+{
+    ROS_INFO("Wifi constructed");
+}
 
-// Wifi::~Wifi()
-// {
-//     ROS_INFO("Wifi destructed");
-// }
+Wifi::~Wifi()
+{
+    ROS_INFO("Wifi destructed");
+}
+/* send on behalf of controller */
+void Wifi::send(const string& _hint)
+{
 
-// void Wifi::recv_callback(wifi::WifiIO::ConstPtr _msg)
-// {
-//     this->que.push(_msg);
-// }
-// bool Wifi::is_msg_valid(wifi::WifiIO::ConstPtr _msg)
-// {
-//     //check
-//     if(_msg){
-//         return _msg->error_code == WIFI_ERR_NONE;
-//     }
-//     else
-//         return false;
-// }
-// wifi::WifiIO::ConstPtr Wifi::pop()
-// {
-//     while(!this->que.empty()){
-//         auto _msg = this->que.front();
-//         if(this->is_msg_valid(_msg)){
-//             this->que.pop();
-//             return _msg;
-//         }
-//         else{
-//             this->que.pop();
-//             ROS_WARN("Discard a wifi msg");
-//         }
-//     }
-//     return nullptr;
-// }
+}
+/* true then can enter, else must wait */
+bool Wifi::node_ocp(const RouteNode& _nd)
+{
+    return false;
+}
+bool Wifi::is_msg_valid(WifiIO::ConstPtr _msg)
+{
+    //check
+    if(_msg){
+        return _msg->error_code == (char)WIFI_ERR_NONE;
+    }
+    else
+        return false;
+}
+WifiIO::ConstPtr Wifi::pop()
+{
+    while(!this->que.empty()){
+        auto _msg = this->que.front();
+        if(this->is_msg_valid(_msg)){
+            this->que.pop();
+            return _msg;
+        }
+        else{
+            this->que.pop();
+            ROS_WARN("Discard a wifi msg");
+        }
+    }
+    return nullptr;
+}
+void Wifi::recv_callback(WifiIO::ConstPtr _msg)
+{
+    while(this->que.size() >= WIFI_BUFF_SIZE)
+        this->que.pop();
+    this->que.push(_msg);
+}
